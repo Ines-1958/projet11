fc5c8b3a925acd97f2fd374901803729ddab7c52
Ines-1958 <79308453+Ines-1958@users.noreply.github.com>
2022-09-18 18:06:21 +0200

    Updates

---
+++ precache-manifest.d72f44eaa4b6c944eb47c5f8801fee2b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "93f9af9f76f8b3f40ead",
+    "url": "/projet11/static/css/main.dc8fc64d.chunk.css"
+  },
+  {
+    "revision": "93f9af9f76f8b3f40ead",
+    "url": "/projet11/static/js/main.93f9af9f.chunk.js"
+  },
+  {
+    "revision": "97bc1625e33b04eb7828",
+    "url": "/projet11/static/js/1.97bc1625.chunk.js"
+  },
+  {
+    "revision": "a7ec3417f538afc3cc37",
+    "url": "/projet11/static/js/2.a7ec3417.chunk.js"
+  },
+  {
+    "revision": "55e14490e30a8c0a6d18",
+    "url": "/projet11/static/js/runtime~main.55e14490.js"
+  },
+  {
+    "revision": "37bc720084ada58e780fbc7aa077b46d",
+    "url": "/projet11/static/media/bgApropos.37bc7200.png"
+  },
+  {
+    "revision": "509591ad23863cde4943520e13b97b47",
+    "url": "/projet11/static/media/etoile.509591ad.svg"
+  },
+  {
+    "revision": "0bec8c6df9b9e98cc5d2f21bb33cea62",
+    "url": "/projet11/static/media/etoileCouleur.0bec8c6d.svg"
+  },
+  {
+    "revision": "81575d8d2691f5e261bf0e1b279fc88e",
+    "url": "/projet11/static/media/bgImg.81575d8d.png"
+  },
+  {
+    "revision": "eeb3ca706ca941867e31be8a73843c25",
+    "url": "/projet11/static/media/bgImage.eeb3ca70.png"
+  },
+  {
+    "revision": "f095cd139dc2e1d11de674b7f997f60a",
+    "url": "/projet11/index.html"
+  }
+];
