0f2604f1f7e66dc1af72d92214b396ff3918ec75
Ines-1958 <79308453+Ines-1958@users.noreply.github.com>
2022-09-18 12:10:52 +0200

    clean code

--- src/pages/Apropos/Apropos.jsx
+++ src/pages/Apropos/Apropos.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import '../Apropos/Apropos.css'
 import bgApropos from './bgApropos.png'
-import { useState } from 'react'
 import Accordeon from '../../components/Accordeon/Accordeon'
 
 export default function Apropos() {
-  const [dataApropos, setdataApropos] = useState([
+  const dataApropos = [
     {
       title: 'Fiabilité',
       content:
@@ -26,7 +25,7 @@ export default function Apropos() {
       content:
         "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
     },
-  ])
+  ]
 
   return (
     <div className="container-propos">
