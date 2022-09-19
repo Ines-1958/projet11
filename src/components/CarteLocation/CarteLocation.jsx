daf4a820a90fd9b8a920b29c8c45852dfc9644a4
Ines-1958 <79308453+Ines-1958@users.noreply.github.com>
2022-09-19 13:40:58 +0200

    build json data

--- src/components/CarteLocation/CarteLocation.jsx
+++ src/components/CarteLocation/CarteLocation.jsx
@@ -8,7 +8,7 @@ export default function CarteLocation() {
 
   useEffect(() => {
     const getHome = async () => {
-      const response = await fetch('/donnees-json/logements.json')
+      const response = await fetch('../../donnees-json/logements.json')
 
       if (!response.ok) {
         const message = `An error has occured: ${response.status}`
